41ce92b869889e94c1daff7832b6324df30f289e
203455 <77992738+203455@users.noreply.github.com>
2022-09-29 14:40:03 -0500

    Update exam.py

--- exam/exam.py
+++ exam/exam.py
@@ -2,50 +2,17 @@ import threading
 import time
 import random
 
-mutex = [threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock(),threading.Lock()]
+mutex = threading.Lock()
 
-
-
-def usandoPalillos(id):
-    izquierdo = mutex[id]
-    derecho = mutex[(id+1)%8]
-    
-    
-    izquierdo.acquire() #Ocupa el palillo que le corresponde por defecto
-    
-    if derecho.acquire(blocking=False):
-        return True
-    else: 
-        izquierdo.release() #puesto que no tiene el derecho, libera
-        return False
-
-def dejarPalillos(id):
-    izquierdo = mutex[id]
-    derecho = mutex[(id+1)%8]
-    
-    izquierdo.release()
-    derecho.release()
 
 def crito(id):
-    i = False
-    iter = 0
-    
-    while i == False:
-        enUso = usandoPalillos(id)
-    
-        if enUso:
-            print("Persona: "+str(id+1)+" comiendo")
-            print("Persona: "+str(id+1)+" uso palillos: "+str(id+1)+" y "+str(((id+1)%8)+1))
-            time.sleep(random.randint(2, 5))
-            dejarPalillos(id)
-            print("Persona: "+str(id+1)+" terminó de comer")
-            i==True
-            break
-        else:
-            if iter < 5:
-                print("Persona: "+str(id+1)+" esperando")
-                iter=iter+1
-                
+    global x;
+    x = x-2
+    print("Persona "+str(id+1)+" está comiendo")
+    print("palillos en espera (mano izquierda de otro comensal): "+str(x))
+    time.sleep(random.randint(3,5))
+    print("Persona "+str(id+1)+" terminó de comer")
+    x = 8
         
         
 
@@ -55,12 +22,13 @@ class Persona(threading.Thread):
         self.id=id
 
      def run(self):
-        #mutex.acquire() #Inicializa semáforo , lo adquiere
+        mutex.acquire() #Inicializa semáforo , lo adquiere
         crito(self.id)
-        #mutex.release() #Libera un semáforo e incrementa la varibale
+        mutex.release() #Libera un semáforo e incrementa la varibale
 
 hilos = [Persona(0), Persona(1), Persona(2), Persona(3), Persona(4), Persona(5), Persona(6), Persona(7)]
 
+x=8;
 
 for h in hilos:
     h.start()
